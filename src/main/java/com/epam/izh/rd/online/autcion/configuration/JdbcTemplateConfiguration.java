4de055b9d2fbbfd9891fbbe9546c3ccc92bd4444
Revol32 <72274775+Revol32@users.noreply.github.com>
2021-01-11 01:33:13 +0400

    Done

--- src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
+++ src/main/java/com/epam/izh/rd/online/autcion/configuration/JdbcTemplateConfiguration.java
@@ -12,7 +12,6 @@ import javax.sql.DataSource;
 @ConfigurationProperties(prefix = "spring.datasource")
 public class JdbcTemplateConfiguration {
 
-
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
